24bb4e61f938b1c9c33498071ed66085ae1f03e7
Jimmy Dee <jgvdthree@gmail.com>
2022-05-29 07:38:26 -0600

    Initial Commit

---
+++ PhotoAlbumBrowser/PhotoAlbumBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoAlbumBrowserApp.swift
+//  PhotoAlbumBrowser
+//
+//  Created by Jimmy Dee on 5/29/22.
+//
+
+import SwiftUI
+
+@main
+struct PhotoAlbumBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
